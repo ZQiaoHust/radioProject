de4ee022eaa7506398039b018ce89bb762490461
zengqiao <350195410@qq.com>
2016-01-30 22:35:05 +0800

    spectrum real and back

---
+++ spectrumChart/app/src/main/java/com/manyseries/Constant.java
@@ -0,0 +1,22 @@
+package com.manyseries;
+
+import android.support.v4.util.Pair;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by Administrator on 2015/12/5.
+ */
+public class Constant {
+
+    public static Queue<List<int[]>> queue_sweep= new LinkedList<>();
+    public static Queue<int[]> queue_back= new LinkedList<>();
+    public static List<Pair<Double,Double>> list= new ArrayList();
+
+
+
+
+}
